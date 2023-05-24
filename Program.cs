9e24d31623dee141f89c18112243d49448f7596a
Ирина <vredprep@inbox.ru>
2023-05-24 12:02:33 +0500

    пробные выводы консоли

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string hello = "Привет мир";
+Console.WriteLine(hello);
+Console.WriteLine("Добро пожаловать в C#!");
+Console.WriteLine("Пока мир...");
+Console.WriteLine(24.5);
